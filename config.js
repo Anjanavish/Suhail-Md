aaf514f891f302ad358b36a3522759bf2710476d
Anjanavish <138042039+Anjanavish@users.noreply.github.com>
2024-01-20 16:14:18 +0530

    Update config.js

--- config.js
+++ config.js
@@ -18,7 +18,7 @@ global.timezone= process.env.TZ || process.env.TIME_ZONE || "Asia/Karachi";
 global.github=process.env.GITHUB|| "https://github.com/Anjana-Md";
 global.gurl = process.env.GURL  || "https://whatsapp.com/channel/0029VaKtE6S8KMqi1N5g5m0Z";
 global.website =process.env.GURL|| "https://whatsapp.com/channel/0029VaKtE6S8KMqi1N5g5m0Z" ; 
-global.THUMB_IMAGE=process.env.THUMB_IMAGE|| "https://telegra.ph/file/d5b1c3544fedc23e11a06.jpg" ; // SET LOGO FOR IMAGE 
+global.THUMB_IMAGE=process.env.THUMB_IMAGE|| "https://telegra.ph/file\/6e2537989425a0999f8ca.png" ; // SET LOGO FOR IMAGE 
 
 
 
